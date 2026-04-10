1fdb5cfdc1d7e0f21fed7c3234e6299bad3e4d53
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2026-04-11 02:39:18 +0600

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const CTA = () => {
   return (
 	<div className='cta'>
-		<a className='btn' >Download CV</a>
+		<a className='btn'>Download CV</a>
 		<a href='#contact' className='btn btn-primary'>Let's talk</a>
 	</div>
   )
