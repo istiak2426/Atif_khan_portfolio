77fa71b54a8281f085ff878ec3f32f14992319eb
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2026-04-11 02:15:49 +0600

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from "../../assets/cv.pdf"
+
 
 const CTA = () => {
   return (
 	<div className='cta'>
-		<a href="https://atifhossainkhan.vercel.app/" download className='btn' >Download CV</a>
+		<a className='btn' >Download CV</a>
 		<a href='#contact' className='btn btn-primary'>Let's talk</a>
 	</div>
   )
