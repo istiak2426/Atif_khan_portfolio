d428f4ba487c155582d3f6b9b10b4cfeea62f05b
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2026-04-11 02:50:11 +0600

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,9 +8,9 @@ import { BsFacebook} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
 	<div className='header__socials'>
-		<a href="https://www.linkedin.com/in/istiak2426" target='_blank' rel="noopener noreferrer" ><BsLinkedin/></a>
-		<a href="https://github.com/istiak2426" target='_blank'  rel="noopener noreferrer"><BsGithub/></a>
-		<a href="https://www.facebook.com/istiak2426" target='_blank' rel="noopener noreferrer"><BsFacebook/></a>
+		<a href="https://atifhossainkhan.vercel.app/" target='_blank' rel="noopener noreferrer" ><BsLinkedin/></a>
+		<a href="https://atifhossainkhan.vercel.app/#" target='_blank'  rel="noopener noreferrer"><BsGithub/></a>
+		<a href="https://atifhossainkhan.vercel.app/#" target='_blank' rel="noopener noreferrer"><BsFacebook/></a>
 	</div>
   )
 }
